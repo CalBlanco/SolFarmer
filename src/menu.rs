6fb0ac2d9439289e4578bd54b9bb20e41fc4521e
WeekieNHN <frankpodraza@icloud.com>
2024-05-17 13:58:46 -0700

    added menu title, changed fonts

--- src/menu.rs
+++ src/menu.rs
@@ -114,6 +114,22 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..default()
         })
         .with_children(|parent| {
+                // Add the title text
+                parent.spawn(TextBundle {
+                    text: Text::from_section(
+                        "SolFarmer",
+                        TextStyle {
+                            font: asset_server.load("fonts/Disolve_light.ttf"),
+                            font_size: 60.0,
+                            color: Color::WHITE,
+                        },
+                    ),
+                    style: Style {
+                        margin: UiRect::bottom(Val::Px(50.0)), // Add margin to create space below the title
+                        ..default()
+                    },
+                    ..default()
+                });
             parent
                 .spawn((PlayButton, ButtonBundle {
                     style: Style {
@@ -135,7 +151,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     parent.spawn((PlayButton, TextBundle::from_section(
                         "Play",
                         TextStyle {
-                            font: asset_server.load("fonts/EvilEmpire.otf"),
+                            font: asset_server.load("fonts/BebasNeue-Regular.ttf"),
                             font_size: 40.0,
                             color: Color::rgb(0.9, 0.9, 0.9),
                         },
@@ -164,7 +180,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     parent.spawn((QuitButton, TextBundle::from_section(
                         "Quit",
                         TextStyle {
-                            font: asset_server.load("fonts/EvilEmpire.otf"),
+                            font: asset_server.load("fonts/BebasNeue-Regular.ttf"),
                             font_size: 40.0,
                             color: Color::rgb(0.9, 0.9, 0.9),
                         },
