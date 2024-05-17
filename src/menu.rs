126e66df7e5541acc399a2ad6a8f519cf3a91125
WeekieNHN <frankpodraza@icloud.com>
2024-05-17 11:52:34 -0700

    Added quit button

--- src/menu.rs
+++ src/menu.rs
@@ -1,23 +1,29 @@
 //! This example illustrates how to create a button that changes color and text based on its
 //! interaction state.
 
-use bevy::prelude::*;
+use bevy::{app::AppExit, prelude::*};
 
 use super::AppState;
 
 pub fn build_plugin(app: &mut App) {
     app
         .add_systems(OnEnter(AppState::Menu), setup)
-        .add_systems(Update, (button_system).run_if(in_state(AppState::Menu)))
+        .add_systems(Update, (play_button_system).run_if(in_state(AppState::Menu)))
+        .add_systems(Update, (quit_button_system).run_if(in_state(AppState::Menu)))
         .add_systems(OnExit(AppState::Menu), cleanup);
 }
 
+#[derive(Component)]
+struct PlayButton;
+
+#[derive(Component)]
+struct QuitButton;
 
 const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
 const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
 
-fn button_system(
+fn play_button_system(
     mut interaction_query: Query<
         (
             &Interaction,
@@ -25,13 +31,14 @@ fn button_system(
             &mut BorderColor,
             &Children,
         ),
-        (Changed<Interaction>, With<Button>),
+        (Changed<Interaction>, With<PlayButton>),
     >,
-    mut text_query: Query<&mut Text>,
+    mut text_query: Query<&mut Text, With<PlayButton>>,
     mut game_state: ResMut<NextState<AppState>>,
 ) {
-    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
-        let mut text = text_query.get_mut(children[0]).unwrap();
+    // for (interaction, mut color, mut border_color, children) in &mut interaction_query
+    for (interaction, mut color, mut border_color, _children) in interaction_query.iter_mut() {
+        let mut text = text_query.get_single_mut().unwrap();
         match *interaction {
             Interaction::Pressed => {
                 text.sections[0].value = "Play".to_string();
@@ -46,6 +53,44 @@ fn button_system(
             }
             Interaction::None => {
                 text.sections[0].value = "Play".to_string();
+                *color = NORMAL_BUTTON.into();
+                border_color.0 = Color::BLACK;
+            }
+        }
+    }
+}
+
+fn quit_button_system(
+    mut interaction_query: Query<
+        (
+            &Interaction,
+            &mut BackgroundColor,
+            &mut BorderColor,
+            &Children,
+        ),
+        (Changed<Interaction>, With<QuitButton>),
+    >,
+    mut app_exit_events: EventWriter<AppExit>,
+    mut text_query: Query<&mut Text, With<QuitButton>>,
+    mut game_state: ResMut<NextState<AppState>>,
+) {
+    // for (interaction, mut color, mut border_color, children) in &mut interaction_query
+    for (interaction, mut color, mut border_color, _children) in interaction_query.iter_mut() {
+        let mut text = text_query.get_single_mut().unwrap();
+        match *interaction {
+            Interaction::Pressed => {
+                text.sections[0].value = "Quit".to_string();
+                *color = PRESSED_BUTTON.into();
+                border_color.0 = Color::RED;
+                app_exit_events.send(AppExit);
+            }
+            Interaction::Hovered => {
+                text.sections[0].value = "Quit".to_string();
+                *color = HOVERED_BUTTON.into();
+                border_color.0 = Color::GOLD;
+            }
+            Interaction::None => {
+                text.sections[0].value = "Quit".to_string();
                 *color = NORMAL_BUTTON.into();
                 border_color.0 = Color::BLACK;
             }
@@ -69,7 +114,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         })
         .with_children(|parent| {
             parent
-                .spawn(ButtonBundle {
+                .spawn((PlayButton, ButtonBundle {
                     style: Style {
                         width: Val::Px(150.0),
                         height: Val::Px(65.0),
@@ -83,16 +128,44 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     border_color: BorderColor(Color::BLACK),
                     background_color: NORMAL_BUTTON.into(),
                     ..default()
-                })
+                }))
                 .with_children(|parent| {
-                    parent.spawn(TextBundle::from_section(
+                    parent.spawn((PlayButton, TextBundle::from_section(
                         "Play",
                         TextStyle {
                             font: asset_server.load("fonts/EvilEmpire.otf"),
                             font_size: 40.0,
                             color: Color::rgb(0.9, 0.9, 0.9),
                         },
-                    ));
+                    )));
+                });
+        })
+        .with_children(|parent| {
+            parent
+                .spawn((QuitButton, ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    border_color: BorderColor(Color::BLACK),
+                    background_color: NORMAL_BUTTON.into(),
+                    ..default()
+                }))
+                .with_children(|parent| {
+                    parent.spawn((QuitButton, TextBundle::from_section(
+                        "Quit",
+                        TextStyle {
+                            font: asset_server.load("fonts/EvilEmpire.otf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    )));
                 });
         });
 }
